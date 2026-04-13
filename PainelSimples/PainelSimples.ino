8554832eb013389c06da6896628e71ae37677094
Luciano Ramalho <luciano@ramalho.org>
2026-04-13 16:29:03 -0300

    painel B com reversao

--- PainelSimples/PainelSimples.ino
+++ PainelSimples/PainelSimples.ino
@@ -7,27 +7,58 @@ const uint8_t LDR_PIN    = A0;
 const uint8_t BUZZER_PIN = 13;
 const uint8_t RED_PIN    = 2;
 const uint8_t GREEN_PIN  = 3;
-const uint8_t BUTTON_PIN = A1;
-const uint8_t LINE_RELAY_PIN = 11;
+const uint8_t MAIN_TRACK_BUTTON_PIN = A1;
+const uint8_t MAIN_TRACK_RELAY_PIN = 11;
+const uint8_t REVERSE_INPUT_PIN = A2;
+const uint8_t REVERSE_RELAY_PIN = 9;
 
 
 DigitalOutput redLed(RED_PIN, HIGH);
 DigitalOutput greenLed(GREEN_PIN, HIGH);
-Button powerBtn(BUTTON_PIN, LOW);
-DigitalOutput lineRelay(LINE_RELAY_PIN, LOW);
+Button powerBtn(MAIN_TRACK_BUTTON_PIN, LOW);
+DigitalOutput mainTrackRelay(MAIN_TRACK_RELAY_PIN, LOW);
+Button reverseInput(REVERSE_INPUT_PIN, LOW);
+DigitalOutput reverseRelay(REVERSE_RELAY_PIN, LOW);
 Buzzer buzzer(BUZZER_PIN);
 
 bool lineActive = false;
+bool reverseActive = false;
+
+void handlePower();
+void handleReverse();
+void handleShortCircuit();
 
 void setup() {
   pinMode(LDR_PIN, INPUT);
+}
+
+void updateControls() {
+  powerBtn.update();    // required for debounce
+  reverseInput.update();  // required for debounce
+  redLed.update();      // required to blink on background
+  buzzer.update();      // required for timed tones
+}
+
+void loop() {
+  handlePower();
+  handleReverse();
+  handleShortCircuit();
+  updateControls();
+  delay(LOOP_DELAY);
 }
 
 void handlePower() {
   if (powerBtn.justPressed()) {
     lineActive = !lineActive;
     greenLed.set(lineActive);
-    lineRelay.set(lineActive);
+    mainTrackRelay.set(lineActive);
+  }
+}
+
+void handleReverse() {
+  if (reverseInput.justPressed()) {
+    reverseActive = !reverseActive;
+    reverseRelay.set(reverseActive);
   }
 }
 
@@ -47,13 +78,4 @@ void handleShortCircuit() {
         greenLed.turnOn();
     }
   }
-}
-
-void loop() {
-  handlePower();
-  handleShortCircuit();
-  powerBtn.update();  // required for debounce
-  redLed.update();    // required to blink
-  buzzer.update();    // required for timed tones
-  delay(LOOP_DELAY);
 }
